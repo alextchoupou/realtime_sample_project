d7469e52732efcfbbf05c7baf2426a20967003fb
alex <alextchoupou@gmail.com>
2024-11-03 07:56:13 +0100

    Configuring and installing Apache Flink with Docker

--- file.py
+++ file.py
@@ -6,8 +6,7 @@ from datetime import datetime
 from faker import Faker
 from confluent_kafka import SerializingProducer
 
-
-faker =  Faker()
+faker = Faker()
 
 
 def generate_sales_transactions():
@@ -20,11 +19,12 @@ def generate_sales_transactions():
         "productPrice": round(random.uniform(10, 1000), 2),
         "productQuantity": random.randint(1, 10),
         "productBrand": random.choice(["Apple", "Samsung", "oneplus", "mi", "boat", "sony"]),
-        "currency": random.choice(['USD','GBP']),
+        "currency": random.choice(['USD', 'GBP']),
         "customerId": user['username'],
         "transactionDate": datetime.utcnow().isoformat() + "Z",
         "paymentMethod": random.choice(['credit_card', 'debit_card', 'online_transfer'])
     }
+
 
 def delivery_report(err, msg):
     if err is not None:
@@ -32,26 +32,27 @@ def delivery_report(err, msg):
     else:
         print(f"Message delivered to the {msg.topic()}[{msg.partition()}] at offset {msg.offset()}")
 
+
 def main():
     topic = 'financial_transaction'
-    producer = SerializingProducer ({
-            'bootstrap.servers': 'localhost:9092',
-            'key.serializer': lambda v, ctx: str(v).encode('utf-8'),
-            'value.serializer': lambda v, ctx: json.dumps(v).encode('utf-8')
-        })
-    current_time  = datetime.now()
+    producer = SerializingProducer({
+        'bootstrap.servers': 'localhost:9092',
+        'key.serializer': lambda v, ctx: str(v).encode('utf-8'),
+        'value.serializer': lambda v, ctx: json.dumps(v).encode('utf-8')
+    })
+    current_time = datetime.now()
 
     while (datetime.now() - current_time).seconds < 120:
         try:
             transaction = generate_sales_transactions()
-            transaction['totalAmount'] = transaction['productPrice']*transaction['productQuantity']
+            transaction['totalAmount'] = transaction['productPrice'] * transaction['productQuantity']
             print(transaction)
 
-            producer.produce(topic, 
-                             key = transaction['transactionId'],
-                             value = json.dumps(transaction),
-                             on_delivery= delivery_report
-                               )
+            producer.produce(topic,
+                             key=transaction['transactionId'],
+                             value=json.dumps(transaction),
+                             on_delivery=delivery_report
+                             )
             producer.poll(0)
 
             time.sleep(1)
@@ -60,8 +61,9 @@ def main():
             print("Buffer Error! Waiting...")
             time.sleep(0.5)
 
-        except Exception as e :
+        except Exception as e:
             print(e)
+
 
 if __name__ == "__main__":
     main()
